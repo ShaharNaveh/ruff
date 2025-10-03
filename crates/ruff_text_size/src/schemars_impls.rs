7a86a478cc860062349de0cfa78b0d06714b25cb
ShaharNaveh <50263213+ShaharNaveh@users.noreply.github.com>
2025-10-04 01:57:10 +0300

    Copy code from @ CodeMan62 without shame

--- crates/ruff_text_size/src/schemars_impls.rs
+++ crates/ruff_text_size/src/schemars_impls.rs
@@ -6,28 +6,29 @@
 //! bindings to the Workspace API
 
 use crate::{TextRange, TextSize};
-use schemars::{JsonSchema, r#gen::SchemaGenerator, schema::Schema};
+use schemars::{JsonSchema, Schema, SchemaGenerator};
+use std::borrow::Cow;
 
 impl JsonSchema for TextSize {
-    fn schema_name() -> String {
-        String::from("TextSize")
+    fn schema_name() -> Cow<'static, str> {
+        "TextSize".into()
     }
 
-    fn json_schema(r#gen: &mut SchemaGenerator) -> Schema {
+    fn json_schema(generator: &mut SchemaGenerator) -> Schema {
         // TextSize is represented as a raw u32, see serde_impls.rs for the
         // actual implementation
-        <u32>::json_schema(r#gen)
+        <u32>::json_schema(generator)
     }
 }
 
 impl JsonSchema for TextRange {
-    fn schema_name() -> String {
-        String::from("TextRange")
+    fn schema_name() -> Cow<'static, str> {
+        "TextRange".into()
     }
 
-    fn json_schema(r#gen: &mut SchemaGenerator) -> Schema {
+    fn json_schema(generator: &mut SchemaGenerator) -> Schema {
         // TextSize is represented as (TextSize, TextSize), see serde_impls.rs
         // for the actual implementation
-        <(TextSize, TextSize)>::json_schema(r#gen)
+        <(TextSize, TextSize)>::json_schema(generator)
     }
 }
